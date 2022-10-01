c7095ddb8e1752a156fde3eff632ac041a3a2601
Ved Gupta <vedgupta@protonmail.com>
2022-10-01 17:36:06 +0530

    Gradio Web Implementation of Whisper OpenAI automatic speech recognition (ASR) system

---
+++ app.py
@@ -0,0 +1,42 @@
+import whisper
+model = whisper.load_model("base")
+print(model.device)
+
+import gradio as gr 
+import time
+
+def SpeechToText(audio):
+    if audio == None : return "" 
+    time.sleep(1)
+
+    audio = whisper.load_audio(audio)
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # Detect the Max probability of language ?
+    _, probs = model.detect_language(mel)
+    print(f"Language: {max(probs, key=probs.get)}")
+
+    #  Decode audio to Text
+    options = whisper.DecodingOptions(fp16 = False)
+    result = whisper.decode(model, mel, options)
+    return result.text
+
+
+gr.Interface(
+    title = 'OpenAI Whisper implementation on Gradio Web UI', 
+    fn=SpeechToText, 
+    
+    inputs=[
+        gr.Audio(source="microphone", type="filepath")
+    ],
+    outputs=[
+        "textbox"
+    ],
+    live=True
+).launch(
+    debug=False,
+    share=True
+)
